e84fe51b3f77323c2ef75bb2f611c0b2d5495ef5
MIHA DEVOPS <mihaontech@gmail.com>
2024-05-05 16:41:40 +0200

    Changed WagmiProvider to WagmiConfig wrapper usage

--- src/Web3ModalProvider.jsx
+++ src/Web3ModalProvider.jsx
@@ -1,37 +1,55 @@
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { WagmiConfig } from "wagmi";
+import {
+	arbitrum,
+	avalanche,
+	bsc,
+	fantom,
+	gnosis,
+	mainnet,
+	optimism,
+	polygon,
+} from "wagmi/chains";
 
-import { WagmiProvider } from 'wagmi'
-import { arbitrum, mainnet } from 'wagmi/chains'
+const chains = [
+	mainnet,
+	polygon,
+	avalanche,
+	arbitrum,
+	bsc,
+	optimism,
+	gnosis,
+	fantom,
+];
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const projectId = 'a35d6c79f8d84d3c4c57042ca4b28961'
 
-const projectId = import.meta.env.PROJECT_ID;
-const url = import.meta.env.METADATA_URL;
-
-const queryClient = new QueryClient()
 const metadata = {
   name: 'Web3Modal',
   description: 'Devico Test Solution',
-  url: url, 
+  url: 'https://localhost', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
-const chains = [mainnet, arbitrum]
+
 const config = defaultWagmiConfig({
   chains,
   projectId,
   metadata,
 })
 
+// 3. Create modal
 createWeb3Modal({
   wagmiConfig: config,
   projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
 })
 
 export default function Web3ModalProvider({ children }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </WagmiProvider>
+    <WagmiConfig config={config}>
+      {children}
+    </WagmiConfig>
   )
 }
